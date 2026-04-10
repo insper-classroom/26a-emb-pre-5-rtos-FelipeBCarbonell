178d7b243c3cfaa327c97f0c61bd1f297f1cf7df
Felipe Carbonell <fbcarbonell22@gmail.com>
2026-04-10 09:17:39 -0300

    4

--- exe4/main.c
+++ exe4/main.c
@@ -14,10 +14,14 @@ const int LED_PIN_G = 6;
 
 QueueHandle_t xQueueButId;
 SemaphoreHandle_t xSemaphore_r;
+SemaphoreHandle_t xSemaphore_g;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
         xSemaphoreGiveFromISR(xSemaphore_r, 0);
+    }
+    elseif (events == 0x8) { // fall edge
+        xSemaphoreGiveFromISR(xSemaphore_g, 0);
     }
 }
 
@@ -51,6 +55,47 @@ void btn_1_task(void *p) {
     int delay = 0;
     while (true) {
         if (xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE) {
+            if (delay < 1000) {
+                delay += 100;
+            } else {
+                delay = 100;
+            }
+            printf("delay btn %d \n", delay);
+            xQueueSend(xQueueButId, &delay, 0);
+        }
+    }
+}
+
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    int delay = 0;
+
+    while (true) {
+        if (xQueueReceive(xQueueButId, &delay, 0)) {
+            printf("%d\n", delay);
+        }
+
+        if (delay > 0) {
+            gpio_put(LED_PIN_G, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(LED_PIN_G, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+        }
+    }
+}
+
+void btn_2_task(void *p) {
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
+
+    int delay = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_g, pdMS_TO_TICKS(500)) == pdTRUE) {
             if (delay < 1000) {
                 delay += 100;
             } else {
